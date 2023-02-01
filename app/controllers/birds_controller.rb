44798b133f125a3da3a7bcf5d5455317dfef8c3e
Theodore John Josef <john.josef@aol.com>
2023-01-31 22:40:57 -0800

    commit tree c6726b7a3935825ba389c07eb824fef07a6b1835

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -15,5 +15,18 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+  
+  #POST /birds
+  def create 
+    #byebug
+     bird = Bird.create(name: params[:name], 
+      species: params[:species])
+     render json: bird, status: :created
+  end
+
+  #GET /birds 
+  def index 
+    birds = Bird.all
+  end
 
 end
